aa6690b368eda7dad9db8edd41787cab5bdc7b04
henriquemarioto <henrique.marioto@outlook.com>
2022-06-01 18:07:15 -0300

    feat: gradiend on performance graph

--- src/components/BarPerformanceGraph/index.tsx
+++ src/components/BarPerformanceGraph/index.tsx
@@ -1,20 +1,49 @@
-import { Bar, Chart } from "react-chartjs-2";
+import { Chart } from "react-chartjs-2";
+import type { ChartData, ChartArea } from "chart.js";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
 import "chart.js/auto";
 import { Container, ContainerGraph, ContainerNumber, Title } from "./style";
 import getGraphData from "../../utils/getGraphData";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const BarPerformanceGraph = () => {
-  const valuesArray: number[] = getGraphData(200000, 1500000, 9);
-
   interface IUserData {
     id: number;
     year: number;
     gain: number;
   }
 
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Tooltip,
+    Legend
+  );
+
+  const chartRef = useRef<ChartJS>(null);
+
+  function createGradient(ctx: CanvasRenderingContext2D, area: ChartArea) {
+    const gradient = ctx.createLinearGradient(0, area.bottom, 0, area.top);
+
+    gradient.addColorStop(1, "#1FAB89");
+    gradient.addColorStop(0.5, "#1FAB8950");
+    gradient.addColorStop(0, "#1FAB8900");
+
+    return gradient;
+  }
+
   const alterGraphData = () => {
-    return valuesArray.map((item, i) => {
+    return getGraphData(200000, 1500000, 9).map((item, i) => {
       return {
         id: i + 1,
         year: 2014 + i,
@@ -24,6 +53,41 @@ const BarPerformanceGraph = () => {
   };
 
   const [userData, setUserData] = useState<IUserData[]>(alterGraphData());
+  const [chartData, setChartData] = useState<ChartData<any>>({
+    datasets: []
+  });
+
+  useEffect(() => {
+    const chart = chartRef.current;
+
+    if (!chart) {
+      return;
+    }
+
+    setChartData({
+      labels: userData.map((item) => item.year),
+      datasets: [
+        {
+          label: "Dinheiro",
+          data: userData.map((item) => item.gain),
+          borderColor: "#1FAB89",
+          backgroundColor: createGradient(
+            chartRef.current.ctx,
+            chart.chartArea
+          ),
+          fill: "start",
+          tension: 0.3,
+        },
+        {
+          type: "bar",
+          label: "Dinheiro",
+          data: userData.map((item) => item.gain),
+          backgroundColor: ["#2D3748"],
+          borderRadius: 5,
+        },
+      ],
+    });
+  }, [userData]);
 
   return (
     <Container onClick={() => setUserData(alterGraphData())}>
@@ -31,35 +95,12 @@ const BarPerformanceGraph = () => {
 
       <ContainerGraph>
         <Chart
-          data={{
-            labels: userData.map((item) => item.year),
-            datasets: [
-              {
-                type: "line",
-                label: "Dinheiro",
-                data: userData.map((item) => item.gain),
-                backgroundColor: ["#1FAB8949"],
-                borderColor: "#1FAB89",
-                fill: true,
-                tension: 0.3,
-              },
-              {
-                type: "bar",
-                label: "Dinheiro",
-                data: userData.map((item) => item.gain),
-                backgroundColor: ["#2D3748"],
-                borderRadius: 5,
-              },
-            ],
-          }}
+          ref={chartRef}
+          type={"line"}
+          data={chartData}
           options={{
             maintainAspectRatio: false,
             responsive: true,
-            scales: {
-              y: {
-                beginAtZero: true,
-              },
-            },
           }}
         />
         <ContainerNumber>
